b19c8d0c8a55c693c889b2a8a85ff71a448233da
AndyRosenberg <andynaterosenberg@gmail.com>
2020-08-23 20:56:19 -0700

    Loading cleanup

--- config/initializers/init.rb
+++ config/initializers/init.rb
@@ -9,10 +9,12 @@ require_relative "roda_ext"
 
 Roda.class_eval { render_plugins }
 
-@required_files ||= Dir[
-  File.join(__dir__, "../../controllers/*.rb"),
-  File.join(__dir__, "../../models/*.rb"),
-  File.join(__dir__, "../../jobs/*.rb")
-]
+def dirload(dir)
+  File.join(__dir__, "../../#{dir}/*.rb")
+end
 
-@required_files.each { |file| require file }
+Dir.glob([
+  dirload("controllers"),
+  dirload("models"),
+  dirload("jobs")
+]).each { |file| require file }
